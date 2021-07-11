103bb82c12442f4d943cd09694ef279199225fb7
Cyboo <cyborgaming12@gmail.com>
2021-07-11 16:28:34 +0100

    Replace old icon with new icon

--- The4Dimension/ObjectDB/ObjectDBView.Designer.cs
+++ The4Dimension/ObjectDB/ObjectDBView.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ObjectDBView));
             this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
@@ -66,6 +67,7 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.checkBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "ObjectDBView";
